11262c27715c5fd70836d18934baf51b2ddefed8
Alex <amksandr@gmail.com>
2024-10-05 16:23:33 +0300

    BTSH: Added vertical valid ships.

---
+++ simple-js-game/battleship-js/app.js
@@ -0,0 +1,108 @@
+const gamesBoardContainer = document.getElementById('gamesboard-container');
+const optionContainer = document.querySelector('#option-container');
+// *** Buttons
+const flipButton = document.querySelector('#flip-button');
+const refreshButton = document.querySelector('#refresh-button');
+
+let angle = 0;
+
+function flip() {
+  const optionShips = Array.from(optionContainer.children);
+  // if (angle === 0) angle = 90 else angle = 0 
+  angle = angle === 0 ? 90 : 0; 
+
+  optionShips.forEach(ship => ship.style.transform = `rotate(${angle}deg)`);
+}
+
+// *** Create Boards
+const width = 10;
+
+function createBoard(color, user) {
+  const gameBoardContainer = document.createElement('div');
+  gameBoardContainer.classList.add('game-board');
+  gameBoardContainer.style.backgroundColor = color;
+  gameBoardContainer.id = user;
+
+  for (let i = 0; i < width * width; i++) {
+    const block = document.createElement('div');
+    block.id = i;
+    block.classList.add('block');
+    gameBoardContainer.append(block);
+  }
+
+  gamesBoardContainer.append(gameBoardContainer);
+}
+
+createBoard('yellow', 'player');
+createBoard('pink', 'computer'); 
+
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+  }
+}
+
+const destroyer = new Ship('destroyer', 2);
+const submarine = new Ship('submarine', 2);
+const cruiser = new Ship('cruiser', 3);
+const battleship = new Ship('battleship', 4);
+const carrier = new Ship('carrier', 5);
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
+
+function addShipPiece(ship) {
+  const allBoardBlocks = document.querySelectorAll('#computer > div');
+  let randomBoolean = Math.random() < 0.5;
+  let isHorizontal = randomBoolean;
+  let randomStartIndex = Math.floor(Math.random() * width * width);
+  let validStart = isHorizontal 
+    // Last vertical
+    ? randomStartIndex <= width * width - ship.length 
+    ? randomStartIndex : width * width - ship.length : 
+      // handle vertical
+      randomStartIndex <= width * width - width * ship.length ? randomStartIndex :
+       randomStartIndex - ship.length * width + width; 
+
+  let shipBlocks = [];
+
+  for (let i = 0; i < ship.length; i++) {
+    if (isHorizontal) {
+      shipBlocks.push(allBoardBlocks[Number(validStart) + i]);
+    } else {
+      shipBlocks.push(allBoardBlocks[Number(validStart) + i * width]);
+    }
+  }
+
+  let valid;
+
+  if (isHorizontal) {
+    valid = shipBlocks.every((_shipBlock, index) => shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)));
+  } else {
+    valid = shipBlocks.every((_shipBlock, index) => shipBlocks[0].id < 90 + (width * index + 1));
+  }
+
+  const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'));
+
+  if (valid && notTaken) {
+    shipBlocks.forEach(block => {
+      // console.log('BLOCK: ', block);
+      block.classList.add(ship.name);
+      block.classList.add('taken');
+    })
+  } else {
+    addShipPiece(ship)
+  }
+  
+  console.log('Rand: ', randomStartIndex);
+  console.log('s blocks: ', shipBlocks);
+}
+
+
+ships.forEach(ship => addShipPiece(ship));
+
+flipButton.addEventListener('click', flip);
+// refreshButton.addEventListener('click', () => addShipPiece(destroyer));
+
+
+
