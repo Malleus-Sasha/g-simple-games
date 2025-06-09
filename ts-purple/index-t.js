8dd53f87eabeac3c191162e35d9a652b26e6614b
Alex <amksandr@gmail.com>
2025-06-09 12:11:46 +0300

    TSP: part I

--- ts-purple/index-t.js
+++ ts-purple/index-t.js
@@ -30,3 +30,60 @@ const skillsFilter1 = skills.filter((s) => s !== 'DevOps')
     .map(s => s + '! ')
     .reduce((a, b) => a + b);
 console.log(skillsFilter1);
+// Tuples
+const skills2 = [1, 'Dev', 'ops'];
+const skills3 = [1, 'Dev', 'ops', true, true];
+// Readonly
+const ar1 = ['s', 's1', true, true];
+const ar2 = ['s'];
+// let zn1: readonly number = 111; // Error
+// ENUMS
+const resM = {
+    message: 'Success',
+    statusCode: 1, // 1, 2, 3
+};
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 0] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROGRESS"] = 1] = "IN_PROGRESS";
+    StatusCode[StatusCode["FAILED"] = 2] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+var StatusCode2;
+(function (StatusCode2) {
+    StatusCode2[StatusCode2["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode2["IN_PROGRESS"] = "P";
+    StatusCode2["FAILED"] = "F";
+})(StatusCode2 || (StatusCode2 = {}));
+if (resM.statusCode === StatusCode.SUCCESS) {
+    console.log('SUCCESS');
+}
+function actionStatusCode(status) {
+    console.log(status);
+}
+actionStatusCode(1);
+actionStatusCode(StatusCode.FAILED);
+// actionStatusCode('P'); // ERROR
+// FUNCTION
+var Res1Status;
+(function (Res1Status) {
+    Res1Status["Published"] = "published";
+    Res1Status["Draft"] = "draft";
+    Res1Status["Deleted"] = "deleted";
+})(Res1Status || (Res1Status = {}));
+async function getFaqs(req) {
+    const res = await fetch('/faqs', {
+        method: 'POST',
+        body: JSON.stringify(req),
+    });
+    const data = await res.json();
+    return data;
+}
+getFaqs({ topicId: 5, status: Res1Status.Published }).then((res) => {
+    res.map(d => {
+        if (d.status) {
+            console.log(d.status);
+        }
+        ;
+    });
+});
+console.log(Res1Status);
--- ts-purple/index-t.ts
+++ ts-purple/index-t.ts
@@ -41,4 +41,177 @@ const skillsFilter1 = skills.filter((s) => s !== 'DevOps')
 
   // Tuples
   const skills2: [number, string, string] = [1, 'Dev', 'ops'];
+  const skills3: [number, string, string, ...boolean[]] = [1, 'Dev', 'ops', true, true];
+
+// Readonly
+
+const ar1: readonly [string, string, ...boolean[]] = ['s', 's1', true, true];
+const ar2: readonly string[] = ['s'];
+// let zn1: readonly number = 111; // Error
+
+// ENUMS
+
+const resM = {
+  message: 'Success',
+  statusCode: 1, // 1, 2, 3
+}
+
+enum StatusCode {
+  SUCCESS,
+  IN_PROGRESS,
+  FAILED,
+}
+
+enum StatusCode2 {
+  SUCCESS = 1,
+  IN_PROGRESS = 'P',
+  FAILED = 'F',
+}
+
+if (resM.statusCode === StatusCode.SUCCESS) {
+  console.log('SUCCESS');
+}
+
+function actionStatusCode(status: StatusCode) {
+  console.log(status);
+}
+
+actionStatusCode(1);
+actionStatusCode(StatusCode.FAILED);
+// actionStatusCode('P'); // ERROR
+
+// FUNCTION
+enum Res1Status {
+  Published = 'published',
+  Draft = 'draft',
+  Deleted = 'deleted'
+}
+type StatusRes1 = 'published' | 'draft' | 'deleted';
+
+type Req1 = {
+  topicId: 5,
+  status: Res1Status,
+}
+
+type Res1 = {
+  question: string;
+  answer: string;
+  tags: string[];
+  links: number;
+  status?: StatusRes1
+}
+
+async function getFaqs(req: Req1): Promise<Res1[]> {
+  const res = await fetch('/faqs', {
+    method: 'POST',
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+  return data;
+}
+
+getFaqs({topicId: 5, status: Res1Status.Published}).then((res) => {
+  res.map(d => {
+    if(d.status) {
+      console.log(d.status);
+    };
+  })
+})
+
+// Union      (Narrowing)
+function logId(id: string | number) {
+  // Narrowing
+  if (typeof id === 'string') {
+    console.log(id.toLowerCase())
+  }
+  console.log(id);
+}
+
+logId(1);
+logId('log');
+
+// Literals
+enum ReqT1 {
+  GET = 'get',
+  POST = 'post',
+}
+function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
+  return 1;
+}
+
+fetchWithAuth('article', 'post');
+
+// Aliases
+type User = {
+  name: string,
+  age: number,
+  skills: string[]
+}
+
+type Role = {
+  id: number;
+  name: string;
+}
+
+type UserWithRole = User & Role;
+
+// ORE
+type UserFull = {
+  user: User,
+  role: Role
+}
+
+let user1: UserWithRole = {
+  name: 'Al',
+  age: 33,
+  id: 1,
+  skills: ['ad', 'po']
+}
+
+// Interface
+interface UserI {
+  name: string,
+  age: number,
+  skills: string[],
+  log: (id: number) => string, 
+}
+
+interface RoleI {
+  id: number;
+  name: string;
+}
+
+interface UserWithRoleI extends UserI, RoleI {
+  createAt: Date,
+}
+
+// ORE
+interface UserFullI {
+  user: User,
+  role: Role
+}
+
+let user2: UserWithRoleI = {
+  name: 'Al',
+  age: 33,
+  id: 1,
+  skills: ['ad', 'po'],
+  createAt: new Date(),
+  log(id: number) {
+    throw new Error("Function not implemented.");
+  }
+}
+
+// Use
+interface UserDictionary {
+  [index: number]: UserI;
+}
+
+type UserDic = {
+  [index: number]: UserI
+}
+
+// Type Record
+type ud = Record<number, User>;
+
 
